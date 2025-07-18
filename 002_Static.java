d38237bf650cb4b879b8a012835468692c05d818
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-18 07:23:25 +0530

    Update 002_Static.java

--- 002_Static.java
+++ 002_Static.java
@@ -1,15 +1,17 @@
 class StaticMethod{
-    public int add(int a, int b){
-        return (a+b);
+    static int c=5; //Shared by all methods if changes made by one method will reflect for every method
+    public String add(int a, int b){
+        return ("Sum is "+(a+b)+" and the value of c for this method is also become "+c);
     }
     public static int divide(int a, int b){
+        System.out.println("c incremented in divide method from "+c+" to "+(++c));
         return (a/b);
     }
 }
-public class Static {
+class Main {
     public static void main(String[] args) {
-        //For calling static method we do not required class object we can call them using ClassName directly
-        System.out.println(StaticMethod.divide(8,2));
+        //to calling static method not required class object we can call them using ClassName
+        System.out.println("Output of divide method "+StaticMethod.divide(8,2));
         
         //Need object of Class which is not static
         StaticMethod s=new StaticMethod();
