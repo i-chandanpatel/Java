e74075ba6e86c597dcf5cfb51a619504938f3a71
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-19 20:22:58 +0530

    Create InnerClass.java

---
+++ Packages/outerpackage/innerpackage/InnerClass.java
@@ -0,0 +1,7 @@
+package outerpackage.innerpackage;
+
+public class InnerClass {
+    public void showMessage() {
+        System.out.println("Hello from InnerClass (in outerpkg.innerpkg)");
+    }
+}
