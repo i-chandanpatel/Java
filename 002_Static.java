abdc7b22b09d2d04d3a4a13eb6df2f538f2d76fb
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-08-29 12:45:49 +0530

    Update 002_Static.java

--- 002_Static.java
+++ 002_Static.java
@@ -1,20 +1,64 @@
-class StaticMethod{
-    static int c=5; //Shared by all methods if changes made by one method will reflect for every method
-    public String add(int a, int b){
-        return ("Sum is "+(a+b)+" and the value of c for this method is also become "+c);
+class StaticMethod {
+    
+    // Static variable: Shared across all instances and accessible in static context
+    static int c = 5;
+
+    // Final static variable: A constant
+    static final double PI = 3.14159;
+
+    // Static block: Runs once when the class is first loaded
+    static {
+        System.out.println("Static block executed. Initial value of c: " + c);
     }
-    public static int divide(int a, int b){
-        System.out.println("c incremented in divide method from "+c+" to "+(++c));
-        return (a/b);
+
+    // Instance method (non-static): Requires object to call
+    public String add(int a, int b) {
+        return "Sum is " + (a + b) + " and current value of static c is " + c;
+    }
+
+    // Static method: Can be called using class name, modifies static variable
+    public static int divide(int a, int b) {
+        System.out.println("c incremented in divide method from " + c + " to " + (++c));
+        return a / b;
+    }
+
+    // Static nested class: Can be instantiated without outer class object
+    static class StaticInner {
+        void show() {
+            System.out.println("Inside static nested class. Value of static c is " + c);
+        }
+    }
+
+    // Static method accessing static constant
+    public static void printPi() {
+        System.out.println("Value of final static constant PI is: " + PI);
     }
 }
-class Main {
+
+public class Main {
     public static void main(String[] args) {
-        //to calling static method not required class object we can call them using ClassName
-        System.out.println("Output of divide method "+StaticMethod.divide(8,2));
         
-        //Need object of Class which is not static
-        StaticMethod s=new StaticMethod();
-        System.out.println(s.add(8,2));
+        // 1. Calling static method directly using class name
+        System.out.println("Output of divide method: " + StaticMethod.divide(8, 2));
+        
+        // 2. Creating object to call instance method
+        StaticMethod s = new StaticMethod();
+        System.out.println(s.add(8, 2));
+
+        // 3. Accessing static nested class
+        StaticMethod.StaticInner inner = new StaticMethod.StaticInner();
+        inner.show();
+
+        // 4. Accessing final static constant and method
+        StaticMethod.printPi();
+
+        // 5. Demonstrating shared nature of static variable
+        StaticMethod anotherInstance = new StaticMethod();
+        System.out.println("Accessing static c from another object: " + StaticMethod.c);
+
+        // 6. Modifying static variable from different object
+        StaticMethod.c = 100;
+        System.out.println("Static c modified to 100. Checking from first object: " + s.add(1, 1));
+        System.out.println("Static c from another object: " + anotherInstance.add(2, 2));
     }
 }
